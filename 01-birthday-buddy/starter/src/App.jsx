b2e3bf238b9be8ef8282694dcb8f1433ede87465
harald <haral@DEVBOOK>
2023-11-18 15:07:19 +0100

    01-birthday-buddy

--- 01-birthday-buddy/starter/src/App.jsx
+++ 01-birthday-buddy/starter/src/App.jsx
@@ -1,4 +1,44 @@
+import data from "./data";
+import { useState } from "react";
+
 const App = () => {
-  return <h2>Birthday Reminder - Starter</h2>;
+	const [people, setPeople] = useState(data);
+
+	const handleIgnorePerson = (id) => {
+		const newPeople = people.filter((person) => person.id != id);
+		setPeople(newPeople);
+	};
+
+	const deletePeople = () => {
+		setPeople([]);
+	};
+
+	return (
+		<>
+			<h2>{people.length} birthdays today</h2>
+			<button className="btn" type="button" onClick={deletePeople}>
+				Delete All
+			</button>
+			<br />
+			{people.map((person) => {
+				const { id, name, age, image } = person;
+				return (
+					<div key={id}>
+						<img style={{ width: "150px" }} src={image} />
+						<p>{name}</p>
+						<p>{age} years</p>
+						<button
+							className="btn"
+							type="button"
+							onClick={() => handleIgnorePerson(id)}
+						>
+							Ignore
+						</button>
+						<br />
+					</div>
+				);
+			})}
+		</>
+	);
 };
 export default App;
